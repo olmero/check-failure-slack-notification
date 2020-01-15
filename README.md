9c73e91c2aeb6490634cae55d7c54d8489791211
Julien May <julien.may@gmail.com>
2020-01-15 09:27:40 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# slack-reporter-action
+# check-failure-slack-notification
+
+GitHub action to report a check failure to Slack.
+
+
+## Usage
+
+- name: Publish Build Status to Slack
+  if: failed()
+  uses: olmero/check-failure-slack-notification@master
+  env:
+    GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }} 
+    SLACK_TOKEN: ${{ secrets.OLMERO_SLACK_BOT_TOKEN }} 
+  with:
+    slack-channel: 'CSFC2LPT2'
---
+++ index.js
@@ -0,0 +1,52 @@
+const core = require('@actions/core');
+const { GitHub, context } = require('@actions/github')
+const { WebClient } = require('@slack/web-api');
+
+function getSha() {
+    if (context.eventName === 'pull_request') {
+        return context.payload.after;
+    }
+
+    return context.sha;
+}
+
+async function getCheckSuite(sha) {
+    const github = new GitHub(process.env.GITHUB_TOKEN)
+
+    const response = await github.checks.listSuitesForRef({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        ref: sha,
+        app_id: 15368
+    });
+
+    const checkSuiteId = response.data.check_suites[0].id;
+
+    return {
+        id: checkSuiteId,
+        url: `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${sha}/checks?check_suite_id=${checkSuiteId}`
+    }
+}
+
+async function sendSlackMessage(channel, checkSuite) {
+    const slack = new Slack(process.env.SLACK_TOKEN);
+
+    return await web.chat.postMessage({
+        channel: channel,
+        text: `Check for <${checkSuite.url}|${context.repo.repo}> failed`,
+        color: '#cc0000'
+    });
+}
+
+async function run() {
+    try {
+        const slackChannel = core.getInput('slack-channel');
+
+        const checkSuite = await getCheckSuite(getSha());
+        return await sendSlackMessage(slackChannel, checkSuite);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
